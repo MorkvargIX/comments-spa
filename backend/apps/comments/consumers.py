cc0ef8d096dbe286a04192da2bf6ade1aa38c3c0
Maksim Tarasenko <keltullis43@gmail.com>
2026-01-21 14:28:53 +0100

    feat(backend): setting up websocket conenction

---
+++ backend/apps/comments/consumers.py
@@ -0,0 +1,9 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class CommentConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
