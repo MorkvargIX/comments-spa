355dc1cb642b13bc1ce4868890edced9fa480b4d
Maksim Tarasenko <keltullis43@gmail.com>
2026-01-23 19:06:41 +0100

    feat(frontend): add main components

---
+++ frontend/src/components/CommentList.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-white p-4 rounded shadow">
+    <p class="text-gray-500">
+      Comment form (placeholder)
+    </p>
+  </div>
+</template>
